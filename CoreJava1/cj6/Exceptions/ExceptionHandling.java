ed55ba546f1b69a5fedd1311ebc22ab7b7722e4f
Laxmi Sharanya <laxmi@example.com>
2026-02-02 13:11:32 +0530

    Add Assessment1 folder

---
+++ CoreJava1/cj6/Exceptions/ExceptionHandling.java
@@ -0,0 +1,83 @@
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        //camelCase → variables & methods
+        //PascalCase (UpperCamelCase) → class & interface names
+
+        /*
+        Exception handling is a way to handle errors that happen while a program is running, so the program doesn’t crash suddenly.
+
+        👉 Without exception handling → program stops
+        👉 With exception handling → program handles the error and continues normally
+         */
+
+        //1.try–catch Block
+
+        int n=10;
+        int m=0;
+
+        try{
+            int cal = n/m;
+        }
+        catch (Throwable e){
+            System.out.println("Divided by Zero");
+        }
+        //2.The finally block always runs
+        finally {
+            System.out.println("This block always runs");
+        }
+
+
+        //3. Throw
+        /*
+            throw new ArithmeticException("Invalid input");
+            Here:
+
+            throw → keyword
+            ArithmeticException → class
+            new ArithmeticException() → object of that class
+
+            So you are:
+            👉 creating an exception object
+            👉 and throwing eit using the throw kyword
+
+            throw throws only one exception
+         */
+
+        int age=1;
+        if (age < 18) {
+            throw new ArithmeticException("Age must be 18 or above");
+            //4. we are using throw , to make new exceptions
+        }
+        fun();
+
+
+        // multiple exceptions
+        try {
+            int temp=10/0;
+            // Code that may throw an exception
+        } catch (ArithmeticException e) {
+            // Code to handle the exception
+        } catch(ArrayIndexOutOfBoundsException e){
+            // Code to handle the another exception
+        }catch(NumberFormatException e){
+            // Code to handle the another exception
+        }
+    }
+    static int fun() throws ArithmeticException{
+        return 6/0;
+        //throws :
+        //5. If a method uses throws, it does not handle the exception; the caller must handle it using try-catch.
+
+        //If you handle the exception inside the method (using try-catch) → ✅ no throws needed.
+        //If you do NOT handle the exception inside the method → ⚠️ the caller must handle it.
+        //And yes, in that case, you must write throws in the method declaration.
+
+
+
+
+
+    }
+
+    //6.Unchecked (RuntimeException, ArithmeticException)	❌ No
+    //Checked (Exception, IOException, user-defined checked)	✅ Yes
+}
